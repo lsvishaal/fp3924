51ee64aac6d724eac1ffdbf7b293d6197569af6d
lsvishaal <lsvishaal7@gmail.com>
2024-08-04 19:00:51 +0530

    chore: Refactor background styling in App.jsx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <div className="fixed top-0 -z-10 h-full w-full">
       <div className="relative h-full w-full bg-slate-950"><div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>      </div>
       
-      <div className="container mx-auto px-10">
+      <div className="container mx-auto px-10 ">
         <Navbar />
         <Hero />
         <About />
