2074502e3c89e3b57e7994b8a2b224aa479007a9
lsvishaal <lsvishaal7@gmail.com>
2024-08-04 18:58:00 +0530

    chore: Update background styling in App.jsx
    Refactor the background styling in App.jsx to improve readability and consistency. Remove unnecessary absolute positioning and replace it with a relative container. Update the background color and gradients for a more visually appealing design.

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -9,9 +9,7 @@ export default function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-purple-400 selection:text-purple-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-
-      </div>
+      <div className="relative h-full w-full bg-slate-950"><div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>      </div>
       
       <div className="container mx-auto px-10">
         <Navbar />
