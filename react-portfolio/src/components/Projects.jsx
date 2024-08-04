bf6358c65665b6c0a48828106405a22c1f3673c2
lsvishaal <lsvishaal7@gmail.com>
2024-08-04 15:07:06 +0530

    chore: Refactor Projects component for improved readability and consistency

--- react-portfolio/src/components/Projects.jsx
+++ react-portfolio/src/components/Projects.jsx
@@ -1,24 +1,42 @@
-import {PROJECTS} from '../constants/index'
+import { PROJECTS } from "../constants/index";
 
 const Projects = () => {
   return (
-    <div className='border-b border-neutral-900 pb-4'>
-        <h2 className='my-20 text-center text-4xl'>Projects</h2>
-        <div>{PROJECTS.map((project, index) => (
-            <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-                <div className="w-full lg:w-1/4">
-                
-                <img 
+    <div className="border-b border-neutral-900 pb-4">
+      <h2 className="my-20 text-center text-4xl">Projects</h2>
+      <div>
+        {PROJECTS.map((project, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <div className="w-full lg:w-1/4">
+              <img
                 src={project.image}
                 width={150}
-                height={150} 
-                alt={project.title} />
-                </div>
+                height={150}
+                alt={project.title}
+                className="mb-6 rounded"
+              />
             </div>
+            <div className="w-full max-w-xl lg:w-3/4">
+              <h6 className="mb-2 font-semibold">
+                {project.title}
+                <p className="mb-4 text-neutral-400">
+                  {project.description}
+                </p>
+                {project.technologies.map((tech, index) => (
+                    <span
+                    key={index}
+                    className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </h6>
+            </div>
+          </div>
         ))}
-        </div>   
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
